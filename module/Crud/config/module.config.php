cac0f3f686c763546e4a07587ffcc98c75afecb8
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-04-25 12:09:34 +0200

    Update module.config.php

--- module/Crud/config/module.config.php
+++ module/Crud/config/module.config.php
@@ -72,18 +72,9 @@ return array(
             'Crud\Controller\Crud' =>  Controller\CrudController::class
         ),
     ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'application/view/layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'crud/crud/index' => __DIR__ . '/../view/crud/crud/index.phtml',
-           // 'error/404'               => __DIR__ . '/../view/error/404.phtml',
-           // 'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
+    
+   'view_manager' => array(
+      
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
