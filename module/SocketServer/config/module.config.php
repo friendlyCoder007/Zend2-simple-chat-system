214af7212f5170f2dcde98ac36385e4ffb5b0c04
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-11-16 10:39:06 +0100

    Create module.config.php

---
+++ module/SocketServer/config/module.config.php
@@ -0,0 +1,70 @@
+<?php
+namespace SocketServer;
+
+
+return array(
+    
+          
+    'controllers' => array(
+        
+       'factories'=>array(
+           
+                      
+            'SocketServer\Controller\SocketCli' =>'SocketServer\Factory\SocketCliControllerFactory',
+           
+                       
+             
+        ),
+        
+   
+        
+    ),
+    
+    
+'service_manager'=>array(
+    
+         
+'invokables'=>array(
+    
+ 'messageService'=>'\SocketServer\Service\MessageService',
+        
+),     
+            
+    
+  ),  
+    
+        
+       
+    
+ //simple console mode to run the server//    
+'console' => array(
+    
+ 'router' => array(
+     
+ 'routes' => array(
+     
+ 'open-socket' => array(
+     
+ 'options' => array(
+     
+ 'route' => 'open socket',
+     
+ 'defaults' => array(
+     
+ 'controller' => 'SocketServert\Controller\SocketCli',
+     
+ 'action' => 'open'
+     
+ )
+ )
+ )
+ )
+ )
+ ),
+    
+    
+   
+    
+);
+    
+
