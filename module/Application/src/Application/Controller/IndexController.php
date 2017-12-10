690e074d35d33eb4b9839e18fe624e87d2fa47af
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 15:51:18 +0100

    Update IndexController.php

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,1 +1,13 @@
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
 
