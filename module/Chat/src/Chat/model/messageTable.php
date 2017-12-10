a65165492fcb5285e0fb56c841dec03034ceddeb
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 15:15:02 +0100

    Update messageTable.php

--- module/Chat/src/Chat/model/messageTable.php
+++ module/Chat/src/Chat/model/messageTable.php
@@ -1,1 +1,43 @@
+<?php
+
+namespace Chat\model;
+use Zend\Db\TableGateway\TableGateway;
+
+
+class messageTable {
+   
+  protected  $tableGateway; 
+    
+   public function __construct(TableGateway $tableGateway) {
+        
+    
+     $this->tableGateway = $tableGateway;   
+       
+     
+   }
+
+
+public function fetchAll()
+{
+
+ $resultSet = $this->tableGateway->select();
+
+ return $resultSet;
+ }  
+    
+
+    
+ 
+ public function saveMessage($data){
+     
+     
+     
+ $this->tableGateway->insert($data);    
+     
+     
+ }
+ 
+    
+    
+}
 
