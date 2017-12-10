05446b2f96c4d1ae09e745487f0ecfebcb9c896c
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 14:19:08 +0100

    Update global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,1 +1,44 @@
 
+<?php
+return array(
+ 
+  'navigation' => array(
+      
+   'default' => array(
+       
+      array(
+       'label' => 'Home',
+       'route' => 'home',
+    
+          
+             ),
+        
+   array(
+   'label'=>'Chat' 
+   'route'=>'chat'
+   )
+        
+  
+       ),
+     ),  
+    
+ 'db' => array(
+ 'driver' => 'Pdo',
+ 'dsn' => 'mysql:dbname=dbchat;host=localhost',
+ 'driver_options' => array(
+  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+  ),
+  ),
+    
+    
+   'service_manager' => array(
+       
+   'factories' => array(
+
+  'Zend\Db\Adapter\Adapter'=> 'Zend\Db\Adapter\AdapterServiceFactory',
+
+   ),
+       
+          
+    ),
+);
