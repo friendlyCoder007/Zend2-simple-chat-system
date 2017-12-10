4e480ed547355ffa761a1cef2ed9de84cb06772a
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 15:02:33 +0100

    Update SendForm.php

--- module/Chat/src/Chat/Form/SendForm.php
+++ module/Chat/src/Chat/Form/SendForm.php
@@ -1,1 +1,56 @@
 
+<?php
+namespace Chat\Form;
+use Zend\Form\Form;
+  
+class SendForm extends Form{
+
+    public function __construct($name = null, $options = array()) {
+        
+      parent::__construct($name, $options); 
+        
+      $this->add(array(      
+        
+      'name' => 'message',
+      'attributes' => array(
+      'type' => 'text',
+      'id' => 'messageText',
+      'required' => 'required'    
+       ),   
+      'options' => array(
+      'label' => 'Message',
+       ), 
+          
+      ));  
+    
+      
+      $this->add(array(
+           
+      'name' => 'submit',
+      'attributes' => array(
+      'type' => 'submit',
+      'value' => 'Send'),     
+           
+           
+       ));   
+      
+      
+    $this->add(array(
+    'name' => 'refresh',
+    'attributes' => array(
+    'type' => 'button',
+    'id' => 'btnRefresh',   
+    'value' => 'Refresh')    
+        
+        
+    ));  
+      
+      
+        
+    }
+
+
+    
+    
+    
+}
