5a075b9f49ef8604f760ffeab54112e1f96c97d2
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 15:05:13 +0100

    Update User.php

--- module/Chat/src/Chat/model/User.php
+++ module/Chat/src/Chat/model/User.php
@@ -1,1 +1,37 @@
+<?php
+namespace Chat\model;
+
+class User{
+  
+public $Id;
+
+
+public $name;
+
+
+public $email; 
+
+
+public $password;
+
+    
+ public function exchangeArray($data)
+
+ { 
+   
+ $this->id = (!empty($data['Id'])) ? $data['Id'] : null;
+ $this->name = (!empty($data['name'])) ? $data['name'] : null;
+ $this->email = (!empty($data['email'])) ? $data['email'] : null;
+ $this->password = (!empty($data['password'])) ? $data['password'] : null;
+  
+ }
+
+
+ public function getArrayCopy()
+ {
+ return get_object_vars($this);
+ }
+    
+    
+}
 
