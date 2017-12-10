7970ccaa8dfc2e20b097a9dc59080f2656cef617
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 15:11:13 +0100

    Update message.php

--- module/Chat/src/Chat/model/message.php
+++ module/Chat/src/Chat/model/message.php
@@ -1,1 +1,44 @@
+<?php
+
+namespace Chat\model;
+
+
+class message {
+  
+public $id;
+
+
+public $user_id;
+
+
+public $message; 
+
+
+public $stamp;
+
+    
+ public function exchangeArray($data)
+
+ { 
+   
+ $this->id = (!empty($data['id'])) ? $data['id'] : null;
+ $this->user_id = (!empty($data['user_id'])) ? $data['user_id'] : null;
+ $this->message = (!empty($data['message'])) ? $data['message'] : null;
+ $this->stamp = (!empty($data['stamp'])) ? $data['stamp'] : null;
+  
+ }
+
+
+ 
+ 
+ public function getArrayCopy()
+ {
+ return get_object_vars($this);
+ }   
+    
+    
+ 
+ 
+ 
+}
 
