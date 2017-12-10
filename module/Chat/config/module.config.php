09ff2610f8c336a5bacd42b78122f767e25381e4
friendlyCoder007 <jack_gregory@poczta.onet.pl>
2017-12-10 14:24:59 +0100

    Update module.config.php

--- module/Chat/config/module.config.php
+++ module/Chat/config/module.config.php
@@ -1,1 +1,72 @@
 
+<?php
+namespace Chat;
+
+return array(
+
+ 'router' => array(
+     
+   'routes' => array(
+   
+       
+       
+              'chat' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/chat',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Chat\Controller',
+                        'controller'    => 'Chat',
+                        'action'        => 'index',
+                    ),
+                ),
+                  
+                  
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/:action][/:id]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'=>'[0-9]+'
+                            ),
+                            
+                        ),
+                    ),
+                    
+     
+                    
+                ),
+    
+                
+            ),
+           
+    
+    )
+    ),
+     
+    
+    'controllers' => array(
+        'invokables' => array(
+            'Chat\Controller\Chat'=>'Chat\Controller\ChatController',
+          
+        ),
+    ),
+  
+
+    
+    
+
+      'view_manager' => array(
+      
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+
+)
+;?>
